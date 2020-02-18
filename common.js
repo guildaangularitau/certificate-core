a685c63c9467bfe9b6ce78010737f218644b205b
Lucas Polizeli <lumpolizeli@gmail.com>
2020-02-18 10:33:12 -0300

    feat: update file size

--- common.js
+++ common.js
@@ -1,31 +1,37 @@
-const fs = require('fs')
-const path = require('path')
-const pdf = require('html-pdf')
-const renderer = require('ejs')
+const fs = require("fs");
+const path = require("path");
+const pdf = require("html-pdf");
+const renderer = require("ejs");
 
-function createFile (fileName, htmlContent, baseFolder) {
-  console.log(`Creating PDF file: ${fileName}.pdf at ${path.resolve(baseFolder, `${fileName}.pdf`)}`)
+function createFile(fileName, htmlContent, baseFolder) {
+  console.log(
+    `Creating PDF file: ${fileName}.pdf at ${path.resolve(
+      baseFolder,
+      `${fileName}.pdf`
+    )}`
+  );
   return new Promise((resolve, reject) => {
     pdf
-      .create(htmlContent, { height: '4.84in', width: '9in',timeout: 900000 })
+      .create(htmlContent, { height: "288px", width: "600px", timeout: 900000 })
       .toFile(path.resolve(baseFolder, `${fileName}.pdf`), (err, file) => {
-        if (err) reject(err)
-        resolve(file)
-      })
-  })
+        if (err) reject(err);
+        resolve(file);
+      });
+  });
 }
 
-function createBaseDirectory (baseFolder) {
-  if (!fs.existsSync(path.resolve(baseFolder))) return fs.mkdirSync(baseFolder, { recursive: true })
+function createBaseDirectory(baseFolder) {
+  if (!fs.existsSync(path.resolve(baseFolder)))
+    return fs.mkdirSync(baseFolder, { recursive: true });
 }
 
-function renderTemplate (template, data) {
-  console.log('Rendering template with data', data)
-  return renderer.render(template, data)
+function renderTemplate(template, data) {
+  console.log("Rendering template with data", data);
+  return renderer.render(template, data);
 }
 
 module.exports = {
   createFile,
   createBaseDirectory,
   renderTemplate
-}
+};
